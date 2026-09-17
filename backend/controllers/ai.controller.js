4557f7ddfb678e6f39340df8ca87909f385e70aa
yash0g-dev <raunakyash12@gmail.com>
2026-09-17 19:47:03 +0530

    updated gemini model

--- backend/controllers/ai.controller.js
+++ backend/controllers/ai.controller.js
@@ -38,7 +38,7 @@ export const getCodeReview = asyncHandler(async (req, res) => {
   `;
 
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-flash-lite-latest" });
     
     const result = await model.generateContent(prompt);
     const review = result.response.text();
